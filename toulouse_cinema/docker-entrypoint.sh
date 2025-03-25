fa4d18be4fa28188ab3dc2d8cf2509ec496bf5dc
Massyl Ouaddour <ouaddour.massyl.com>
2025-03-25 10:46:27 +0100

    Refactor docker-entrypoint.sh to streamline cron startup process and remove unnecessary commands

--- toulouse_cinema/docker-entrypoint.sh
+++ toulouse_cinema/docker-entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Démarrage du service cron
-/etc/init.d/cron start
-
 # Exécuter le script une première fois au démarrage
 cd /app && python scripts/update_seances.py >> /var/log/cron.log 2>&1
 
-# Garder le conteneur en vie et le cron actif
-cron -f &
-tail -f /var/log/cron.log 
+# Démarrer cron en premier plan
+cron -f 
